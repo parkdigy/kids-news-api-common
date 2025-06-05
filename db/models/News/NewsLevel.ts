b8a16c7532edf853eb4e5b937932d68e9dc561f2
parkdigy <parkdigy@gmail.com>
2025-06-06 00:04:56 +0900

    save

--- db/models/News/NewsLevel.ts
+++ db/models/News/NewsLevel.ts
@@ -12,16 +12,56 @@ export interface TNewsLevel {
   level: number; // 레벨 // tinyint
   news_date: Date; // 뉴스 날짜 // date
   news_category_id: TNewsCategory['id']; // 뉴스 카테고리 ID // int, FK
+  img_url: string | null; // 이미지 URL // varchar(1024), nullable
   title: string; // 제목 // varchar(100)
   description: string; // 소개 내용 // text
-  words: string | null; // 단어 // text, nullable
-  img_url: string | null; // 이미지 URL // varchar(1024), nullable
+  paragraphs: string; // 문단 // text
+  words: string; // 단어 // text
+  discussions: string; // 토론 // text
+  tests: string; // 문제 // text
   create_date: Date; // 등록일자
   update_date: Date; // 수정일자
 }
 
 export type TNewsLevel$InsertData = TableInsertData<TNewsLevel, 'id', 'words' | 'img_url'>;
 export type TNewsLevel$UpdateData = TableUpdateData<TNewsLevel, 'id' | 'news_id' | 'create_date', 'update_date'>;
+
+export class NewsTestInfo {
+  id!: number;
+  question!: string;
+  options!: string[];
+  answer!: string;
+}
+
+export class NewsDiscussionInfo {
+  id!: number;
+  question!: string;
+  answer!: string;
+}
+
+export class NewsWordInfo {
+  id!: number;
+  word!: string;
+  meaning!: string;
+}
+
+export class NewsParagraphInfo {
+  id!: number;
+  img_url!: string | null;
+  title!: string;
+  content!: string;
+}
+
+export class NewsLevelInfo {
+  level!: number;
+  img_url!: string | null;
+  title!: string;
+  description!: string;
+  paragraphs!: NewsParagraphInfo[];
+  words!: NewsWordInfo[];
+  discussions!: NewsDiscussionInfo[];
+  tests!: NewsTestInfo[];
+}
 
 export default TNewsLevel;
 
